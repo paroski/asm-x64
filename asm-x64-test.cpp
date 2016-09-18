4aaa208fe472dabcd693e5579fdea327df27514d
Drew Paroski <drew@memsql.com>
2016-09-18 01:41:56 +0000

    Update license info

--- asm-x64-test.cpp
+++ asm-x64-test.cpp
@@ -1,35 +1,18 @@
 /**
- * Copyright (c) 2009-2016 Andrew J. Paroski
+ * Copyright (c) 2009-2016 Andrew J. Paroski (https://github.com/paroski)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This source file is subject to the OSI MIT license, that is bundled
+ * with this package in the file LICENSE.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
-/**
  * Copyright (c) 2010-2016 Facebook, Inc. (http://www.facebook.com)
  *
  * This source file is subject to version 3.01 of the PHP license,
- * that is bundled with this package in the file LICENSE, and is
- * available through the world-wide-web at the following url:
+ * that is bundled with this package in the file LICENSE.PHP, and
+ * is available through the world-wide-web at the following url:
  *   http://www.php.net/license/3_01.txt
  *
- * If you did not receive a copy of the PHP license and are unable to
- * obtain it through the world-wide-web, please send a note to
+ * If you did not receive a copy of the PHP license and are unable
+ * to obtain it through the world-wide-web, please send a note to
  * license@php.net so we can mail you a copy immediately.
  */
 
